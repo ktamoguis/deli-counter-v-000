60de888bd4d595e7fa10b52275e633ea149930b4
Karl Amoguis <ktamoguis@gmail.com>
2018-04-26 09:02:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def line(people)
+  linestring="The line is currently:"
+  counter = 1
+  if people.length == 0
+    puts "The line is currently empty."
+  else
+    people.each do |person|
+      linestring = linestring << " #{counter}. #{person}"
+      counter += 1
+    end
+    puts "#{linestring}"
+  end
+end
+
+def take_a_number(array,name)
+  queueno = array.length
+  if array.length == 0
+    puts "Welcome, #{name}. You are number 1 in line."
+    array << name
+  end
+  array
+end
